4f43749edafde6ca08947833a63c7d6f8d9ac771
a.breganov <a.breganov@gmail.com>
2019-04-19 19:42:36 +0300

    Added os.environ.keys message

--- NoRP_bot.py
+++ NoRP_bot.py
@@ -10,22 +10,25 @@ from vk_api.longpoll import VkLongPoll
 # инициализирую всё, что нужно для бота Телеграма
 token = '871642712:AAHyhf9X3FleO9t2g-CEuuFb_3Jq9b7q8Ps'
 PORT = int(os.environ.get('PORT', '8080'))
-# updater = Updater(token)
-# updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=token)
-# updater.bot.set_webhook("https://infinite-dusk-92685.herokuapp.com/" + token)
-# updater.idle()
+
 channel_id = '-1001362117188'
 pp = telegram.utils.request.Request(proxy_url='https://88.204.154.155:8080')
 bot = telegram.Bot(token=token, request=pp)
 
 # initialized all Telegram Bot affilated
-bot.send_message("144635221", "NoRP Bot initialized.")
+bot.send_message("144635221", "NoRP Bot initialized on {}.".format(list(os.environ.keys())))
 
 # инициализирую всё, что нужно для группы
 NORP_ID = "114070332"
 MY_ID = "614415"
 GROUP_TOKEN = "8938e1543970e565e8c61c9fa462ac3444a0d340d037fa7f4eb85ff06730c1531dd364f7c670039beaf92"
 connection = {'key':'', 'server':'', 'ts':''}
+
+def Updater():
+	updater = Updater(token)
+	updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=token)
+	updater.bot.set_webhook("https://infinite-dusk-92685.herokuapp.com/" + token)
+	updater.idle()
 
 def getapi(token):
     vk_session = vk_api.VkApi(token=token, api_version=5.95)
